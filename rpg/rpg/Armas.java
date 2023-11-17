0aededf741dabd8d151fb97a9fc073187edc8a8b
CODIGOAFRX <145802023+CODIGOAFRX@users.noreply.github.com>
2023-11-17 10:23:32 +0100

    Add files via upload

---
+++ rpg/rpg/Armas.java
@@ -0,0 +1,34 @@
+package rpg;
+
+public class Armas {
+    private String Nombre;
+    private int Ataque;
+
+    // constructor
+    public Armas(String nombre, int ataque) {
+        this.Nombre = nombre;
+        this.Ataque = ataque;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public int getAtaque() {
+        return Ataque;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public void setAtaque(int ataque) {
+        Ataque = ataque;
+    }
+
+    @Override
+    public String toString() {
+        return "Armas [Nombre=" + Nombre + ", Ataque=" + Ataque + "]";
+    }
+
+}
